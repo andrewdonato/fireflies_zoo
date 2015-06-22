25505483e775536a5760c530ce60e0d5f98310e5
Andrew Donato <andrew.donato91@gmail.com>
2015-06-21 18:15:46 -0700

    followed along with code along video

--- zoo.js
+++ zoo.js
@@ -2,40 +2,33 @@
 // YOUR CODE: Create your Zoo "object literal" and Animal "constructor" and "prototypes" here.
 //------------------------------------------------------------------------------------------------------------------
 
-function Animal(name, legs) {
+function Animal(name, legs, ssn){
   this.name = name;
   this.legs = legs;
-  // this.identify = function(){
-  //  return "I am a " +  this.name + " with " + this.legs + " legs.";
-  // }
 
-  // Watch the video for this nonsense;
+  // Creates privacy for getting and setting things
   // var ssn = ssn;
   // this.getSSN = function(){
   //   return ssn;
   // }
-
   // this.setSSN = function(ssn){
-  //   ssn = ssn;
+  //   ssn = snn;
   // }
-
 };
 
 Animal.prototype.identify = function(){
-  return "I am a " +  this.name + " with " + this.legs + " legs.";
+  return "I am a " + this.name + " with " + this.legs + " legs."
 }
 
 var Zoo = {
-  init: function(animals){
+  init: function (animals){
     this.animals = animals;
   },
-
   bipeds: function(){
     return this.animals.filter(function(animal){
       return animal.legs === 2;
     });
   },
-
   quadrupeds: function(){
     return this.animals.filter(function(animal){
       return animal.legs === 4;
@@ -43,26 +36,7 @@ var Zoo = {
   }
 };
 
-
-
-class Animal
-
-  attr_accessor :name, :legs
-
-  def initialize(name, legs)
-    @name = name
-    @legs = legs
-  end
-
-  def identify
-    "I am a #{name} with #{legs} legs."
-  end
-end
-
-// # Ruby
-
-// a = [1, 2, 3]
-// a.select {|i| i > 2 }
+// , + this.name + "s have " + this.legs + " legs"
 
 //------------------------------------------------------------------------------------------------------------------
 // DRIVER CODE: Do **NOT** change anything below this point. Your task is to implement code above to make this work.
